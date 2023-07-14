c0fb0d07c50434ac224ec064abe481adf47f3944
Taha <traitaha@gmail.com>
2023-07-13 18:05:10 -0700

    nc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,13 +9,11 @@ int main(void)
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 	putchar(str[i]);
 	i++;
 	}
 	putchar('\n');
-	
-	return (0);
-
+	return (1);
 }
