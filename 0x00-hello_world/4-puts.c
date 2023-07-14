4079f3e456366e3fa6cc916fa416799ba4cf0f7d
Taha <traitaha@gmail.com>
2023-07-13 17:07:00 -0700

    nc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
 /**
- * this function prints only
+ * main - prints only
+ *
+ * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
