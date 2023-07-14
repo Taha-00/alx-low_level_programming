155a46ee9c7c0eae657c96e32bf5080bd5575fae
Taha <traitaha@gmail.com>
2023-07-14 15:50:21 -0700

    nc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints only
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 48;
+	char b = 'a';
+
+	while (a < 57)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
