dc67160ff77d23344be9d6a4eebb95b61a4093d8
Taha <traitaha@gmail.com>
2023-07-15 03:13:25 -0700

    nc

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - prints only
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j;
+	int k;
+
+	while (i <= 57)
+	{
+		j = i;
+		while (j <= 57)
+		{
+			if (j != i)
+			{
+				k = j;
+				while (k <= 57)
+				{		
+					if (k != j && k != i)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						if (i == 55 && j == 56 && k == 57)
+						{
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					k++;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
