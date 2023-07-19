375a83e7f07c3b1bb2f0620eeb4fcd07725084c2
Taha <traitaha@gmail.com>
2023-07-18 17:49:08 -0700

    nc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <unistd.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - prints only
+ * main - calls a function
  *
  * Return: 0
  */
 
+int _putchar(char c);
+
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is zero\n", n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
