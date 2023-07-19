5d280bf6a7de83cba82b433a3e12e043212d6da6
Taha <traitaha@gmail.com>
2023-07-18 19:15:45 -0700

    nc

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * void jack_bauer - a function
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int m, h;
+
+	m = 0;
+	h = 0;
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar(h / 10 + 48);
+			_putchar(h % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
