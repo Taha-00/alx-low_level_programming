5eb9cb6c13ca033ae6be0fa4f41066d703d0644a
Taha <traitaha@gmail.com>
2023-07-18 18:38:38 -0700

    nc

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *print_sign - a function
+ *@n: input
+ * Return: 0
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
