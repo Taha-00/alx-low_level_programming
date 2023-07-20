783159ea9121bc7e4fbc55bce6a736b2504c7997
Taha <traitaha@gmail.com>
2023-07-20 03:34:09 -0700

    nc

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - a function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long j  = 1;
+	long k = 2;
+	long o;
+	
+	printf("1, 2, ");
+	for (i = 0; i < 48; i++)
+	{
+		
+		o = j + k;
+		printf("%ld", o);
+		if (i != 47)
+			printf(", ");
+		j = k;
+		k = o;
+	}
+	printf("\n");
+	return (0);
+
+}
