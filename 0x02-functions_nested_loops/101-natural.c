6e3465c58d11c99b53259b88d9b159b70b4ee7ff
Taha <traitaha@gmail.com>
2023-07-20 03:16:43 -0700

    nc

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -16,11 +16,17 @@ void nat_fun(void)
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0)
+		{
 			s = s + i;
+		}
 		else if (i % 5 == 0)
+		{
 			s = s + i;
+		}
 		else
-
+		{
+		}
 	}
 	printf("%d", s);
+	return;
 }
