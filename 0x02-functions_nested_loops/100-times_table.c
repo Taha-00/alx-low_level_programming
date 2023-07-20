020ca1da33e4846697abbb0962761bdfb76f4658
Taha <traitaha@gmail.com>
2023-07-20 02:46:28 -0700

    nc

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_times_table - a function
+ *
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int r = 0;
+	int c = 0;
+	int m;
+
+	if (n < 0 || n > 15)
+		return;
+	while (r <= n)
+	{
+		c = 0;
+		while (c <= n)
+		{
+			m = r * c;
+			if (m / 10 == 0 && c == 0)
+			{
+				_putchar(m % 10 + 48);
+			}
+			else if (m / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar(m % 10 + 48);
+			}
+			else
+			{
+				_putchar(m / 10 + 48);
+				_putchar(m % 10 + 48);
+			}
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			c++;
+		}
+		_putchar('\n');
+		r++;
+	}
+}
