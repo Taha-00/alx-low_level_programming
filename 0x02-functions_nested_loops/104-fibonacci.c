f3c33ae6a310c50689450b69c35b482101b5bd4d
Taha <traitaha@gmail.com>
2023-07-21 14:45:26 -0700

    nc

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,19 +12,28 @@ int main(void)
 {
 	int i;
 	unsigned long j  = 1;
+	unsigned long j1 = j % 1000000000;
+	unsigned long j2 = j / 1000000000;
 	unsigned long k = 2;
-	unsigned long o;
+	unsigned long k1 = k % 1000000000;
+	unsigned long k2 = k / 1000000000;
+	/*unsigned long o;*/
+	unsigned long o1;
+	unsigned long o2;
 
 	printf("1, 2, ");
 	for (i = 0; i < 96; i++)
 	{
 
-		o = j + k;
-		printf("%lu", o);
+		o1 = (j1 + j2 + k1 + k2) % 1000000000;
+		o2 = (j1 + j2 + k1 + k2) / 1000000000;
+		printf("%lu%lu", o2, o1);
 		if (i != 95)
 			printf(", ");
-		j = k;
-		k = o;
+		j1 = k1;
+		j2 = k2;
+		k1 = o1;
+		k2 = o2;
 	}
 	printf("\n");
 	return (0);
