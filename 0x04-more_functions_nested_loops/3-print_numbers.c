1016e030ea1ed00761b98d20b8da0bf2232ea2fd
Taha <traitaha@gmail.com>
2023-07-21 18:40:21 -0700

    nc

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *print_numbers - a function
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
