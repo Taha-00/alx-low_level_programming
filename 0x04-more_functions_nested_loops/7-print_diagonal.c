2c6082db1af7f42f6326d40414bf4ad78d7b68c9
Taha <traitaha@gmail.com>
2023-07-21 19:18:15 -0700

    nc

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_diagonal - a function
+ * @n: input
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i ; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
