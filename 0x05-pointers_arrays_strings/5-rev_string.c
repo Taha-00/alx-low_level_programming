605d447759573a8f3cb6acaaa47e96438b1748b6
Taha <traitaha@gmail.com>
2023-07-25 16:24:30 -0700

    nc

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *rev_string - a function
+ *@s: input a pointer
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	char r[i + 1];
+
+	for (j = i; j >= 0; j--)
+	{
+		r[i - j] = s[j];
+	}
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = r[j];
+	}
+}
