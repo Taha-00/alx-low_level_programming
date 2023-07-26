634e2532bf823142a395516b9bee71c90edf53de
Taha <traitaha@gmail.com>
2023-07-26 12:23:50 -0700

    nc

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,20 +11,25 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int j;
+	char *start, *end, tmp;
 
+	start = s;
+	end = s;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 
-	char r[10];
-
-	for (j = i; j >= 0; j--)
+	for (j = 0; j < i - 1; j++)
 	{
-		r[i - j] = s[j];
+		end++;
 	}
-	for (j = 0; j <= i; j++)
+	for (j = 0; j < i / 2; j++)
 	{
-		s[j] = r[j];
+		tmp = *end;
+		*end = *start;
+		*start = tmp;
+		start++;
+		end--;
 	}
 }
