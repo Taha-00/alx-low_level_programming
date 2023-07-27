4cf58dfe796786d35b445fb4ce83f660f45e9441
Taha <traitaha@gmail.com>
2023-07-27 12:03:12 -0700

    nc

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,16 +10,23 @@
 int _atoi(char *s)
 {
 	int i = 0;
+	int result = 0;
+	int sign = 1;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			_putchar(s[i]);
+			result = result * 10 + (s[i] - '0');
+			if (i > 0)
+			{
+				if (s[i - 1] == '-')
+					sign = -1;
+			}
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 		i++;
 	}
-	return (0);
+	return (result * sign);
 }
