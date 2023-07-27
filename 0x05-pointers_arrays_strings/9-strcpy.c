57747c75b2a9b3623d921153ced6d276859bad69
Taha <traitaha@gmail.com>
2023-07-27 09:50:16 -0700

    nc

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *_strcpy - a function
+ *@dest: input
+ *@src: input2
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
