55f07f8328a02148a46c411754810d3fc57318f4
Taha <traitaha@gmail.com>
2023-07-28 12:19:05 -0700

    nc

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,6 +18,13 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-	/*dest[i] = '\0';*/
+	if (i < n - 1)
+	{
+	while (dest[i] != '\0')
+	{
+	dest[i] = '\0';
+	i++;
+	}
+	}
 	return (dest);
 }
