d39158f660405e6cf6e66cdba5b58d8999600938
Taha <traitaha@gmail.com>
2023-07-28 12:53:21 -0700

    nc

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,21 +17,21 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] < s2[i])
 		{
-			return(-1);
+			return (-1);
 		}
 		else if (s1[i] > s2[i])
 		{
-			return(1);
+			return (1);
 		}
 		else
 		i++;
 	}
 	if (s1[i] == '\0' && s2[i] == '\0')
-		return(0);
+		return (0);
 	else if (s1[i] == '\0')
-		return(-1);
+		return (-1);
 	else if (s2[i] == '\0')
-		return(1);
+		return (1);
 	else
 		return (0);
 }
