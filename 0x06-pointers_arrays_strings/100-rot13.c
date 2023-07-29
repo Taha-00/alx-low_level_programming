be0d313f041516d8b05523167d7bd8efb8196258
Taha <traitaha@gmail.com>
2023-07-29 09:17:21 -0700

    nc

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,25 +2,28 @@
 /* more headers goes there */
 
 /**
- *leet - a function
+ *rot13 - a function
  *@s: input
  *
  * Return: 0
  */
 
-char *leet(char *s)
+char *rot13(char *s)
 {
-	char sym [] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char eqs [] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char sym[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char eqs[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int i = 0;
 	int j;
 
 	while (s[i] != '\0')
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == sym[j])
+			{
 				s[i] = eqs[j];
+				break;
+			}
 		}
 		i++;
 	}
