8e132225dffbadb1055c1574ada9f9414fe1dae2
Taha <traitaha@gmail.com>
2023-07-29 09:35:15 -0700

    nc

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,30 +9,20 @@
 
 void print_number(int n)
 {
-	int i;
-	int j;
-	int k;
-	int c = n;
-	int m;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
-		n = n * -1;
 		_putchar('-');
+		n1 = -n;
 	}
-	for (i = 1; i < n; i++)
+
+	if (n1 / 10 != 0)
 	{
-		c = c / 10;
-		if (c == 0)
-			break;
+		print_number(n1 / 10);
 	}
-	for (j = 0; j < i; j++)
-	{
-		m = n;
-		for (k = 0; k < i - j - 1; k++)
-		{
-			m = m / 10;
-		}
-		_putchar(m % 10 + '0');
-	}
+	_putchar((n1 % 10) + '0');
 }
+
