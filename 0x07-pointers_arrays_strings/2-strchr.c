2d514649f6c540a204ac28aa74a934ecca4a2ce4
Taha <traitaha@gmail.com>
2023-07-31 08:04:10 -0700

    nc

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *_memcpy - a function
+ *@s: input
+ *@c: input2
+ *
+ * Return: string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p;
+
+	while (s[i] != '\n')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			return (p);
+		}
+		i++;
+	}
+	return (NULL);
+}
