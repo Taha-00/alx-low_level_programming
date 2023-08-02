5892e5c50c7509cbb8eb6b9b841187f8938f1fba
Taha <traitaha@gmail.com>
2023-08-01 19:48:12 -0700

    nc

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+ *print_chessboard - a function
+ *@a: input
+ *
+ * Return: string
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+}
