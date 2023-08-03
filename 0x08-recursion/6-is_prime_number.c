d18c8398565e3cb60fedd12cdd84f8bab9aaa9c5
Taha <traitaha@gmail.com>
2023-08-03 15:29:13 -0700

    nc

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,26 @@
 #include "main.h"
 /* more headers goes there */
 
+
+/**
+ *is_prime_number - a function
+ *@n: input
+ *@fac: in
+ *
+ *Return: no.
+ */
+
+int prima(int n, int fac);
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 1)
+		return (0);
+	prima(n, 2);
+}
+
 /**
  *prima - a function
  *@n: input
@@ -15,21 +35,4 @@ int prima(int n, int fac)
 	if (fac == n)
 		return (1);
 	return (prima(n, fac + 1));
-}
-
-/**
- *is_prime_number - a function
- *@n: input
- *
- *
- *Return: no.
- */
-
-int is_prime_number(int n)
-{
-	if (n == 1)
-		return (1);
-	else if (n < 1)
-		return (0);
-	prima(n, 2);
 }
