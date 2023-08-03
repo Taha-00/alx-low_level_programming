a3b89a9d0ddd855c669dd7f3416c0d878dc2f3f0
Taha <traitaha@gmail.com>
2023-08-03 14:49:57 -0700

    nc

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,30 @@
  *
  *Return: no.
  */
+int sqrtr(int n, int r);
 
 int _sqrt_recursion(int n)
 {
-}	
+	if (n < 0)
+		return (-1);
+	else if ( n == 1)
+		return (1);
+	return (sqrtr(n, n / 2));
+}
+
+int sqrtr(int n, int r)
+{
+	if (r * r == n)
+	{
+		return (r);
+	}
+	else if (r < 1)
+	{
+		return (-1);
+	}
+	else
+	{
+		r--;
+		sqrtr(n, r);
+	}
+}
