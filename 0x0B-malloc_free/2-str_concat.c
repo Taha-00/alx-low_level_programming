a4ed1bf8a4aa23481d6225c516f00d0af959d1a7
Taha <traitaha@gmail.com>
2023-08-09 14:47:57 -0700

    nc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,11 +28,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		s[k] = s1[k];
 	}
-	for (k = i; k < i; k++)
+	for (k = 0; k < j; k++)
 	{
-	s[k] = s2[k];
+	s[k + i] = s2[k];
 	}
-	s[k] = '\0';
+	s[i + j + 1] = '\0';
 	return (s);
 }
 
