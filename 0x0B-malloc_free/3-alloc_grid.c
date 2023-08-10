7d90f4d8b8bab0cf03d76e9ec7762aa75ff0647d
Taha <traitaha@gmail.com>
2023-08-09 18:04:17 -0700

    nc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,13 +19,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int *) * height);
+	arr = malloc(sizeof(*arr) * height);
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		arr[i] = malloc(sizeof(int) * width);
+		arr[i] = malloc(sizeof(**arr) * width);
 		if (arr[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
