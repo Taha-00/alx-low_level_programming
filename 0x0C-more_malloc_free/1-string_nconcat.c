b07923f6d4fd30c61d82c24b269d23c6ec4a2770
Taha <traitaha@gmail.com>
2023-08-11 04:36:02 -0700

    nc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,25 +3,6 @@
 
 /* more headers goes there */
 
-
-/**
- *_strlen - a function
- *@s: input a pointer
- *
- * Return: 0
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-		return (0);
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
 /**
  *string_nconcat - a function
  *@s1: in1
@@ -33,44 +14,36 @@ int _strlen(char *s)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int s1_len = 0, s2_len = 0, i;
 	char *s;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k;
 
-	while (s1[i] != '\0')
-                i++;
+	/* if NULL is passed, treat as an empty string */
 	if (s1 == NULL)
 		s1 = "";
-
-	while (s2[j] != '\0')
-                j++;
 	if (s2 == NULL)
-                s2 = "";
+		s2 = "";
 
-	if (n < j)
-		j = n;
+	/* While at index 0 */
+	while (s1[s1_len] != '\0')
+		s1_len++;
+	while (s2[s2_len] != '\0')
+		s2_len++;
 
-	if (i == 0 && j == 0)
-	{
-		s = malloc(sizeof(char));
-		s[0] = '\0';
-		return (s);
-	}
-	s = malloc(sizeof(char) * (i + n + 1));
+	if (n >= s2_len)
+		n = s2_len;
+
+	/* Memory allocation */
+	s = malloc(sizeof(char) * n + s1_len + 1);
 	if (s == NULL)
 		return (NULL);
 
-	for (k = 0; k < i; k++)
-	{
-		s[k] = s1[k];
-	}
-	for (k = 0; k < j; k++)
-	{
-	s[k + i] = s2[k];
-	}
-	s[i + j + 1] = '\0';
+	for (i = 0; i < s1_len; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		s[i + s1_len] = s2[i];
+
+	s[i + s1_len] = '\0';
+
 	return (s);
 }
-
-
