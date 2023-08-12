a4bb6e57dd42a95ee0ef781c8d77debb61818ec2
Taha <traitaha@gmail.com>
2023-08-11 18:07:46 -0700

    nc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/**
+ *_calloc - a function
+ *@nmemb: in1
+ *@size: in2
+ *
+ *Return: ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *c;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(nmemb * size);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		c[i] = 0;
+	return (c);
+
+}
