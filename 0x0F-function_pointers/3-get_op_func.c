1791b74b455ea7c8578ac68739c1237dee936382
Taha <traitaha@gmail.com>
2023-08-16 19:36:29 -0700

    nc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ *get_op_func - a function
+ *@s: in1
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	return (0);
+}
