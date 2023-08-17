5e66de9b89f6e25824a40e3f98a7a9c37eb3f34c
Taha <traitaha@gmail.com>
2023-08-16 18:32:06 -0700

    nc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,14 @@
-#ifndef SIZE
-#define SIZE 1024
-#endif /*SIZE*/
+#include "function_pointers.h"
+/* more headers goes there */
+
+/**
+ *print_name - a function
+ *@name: in
+ *@f: in2
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
