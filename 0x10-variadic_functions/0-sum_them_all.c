3eb9690343dd17ff831fa2369e5475702512c4e0
Taha <traitaha@gmail.com>
2023-08-17 16:27:44 -0700

    nc

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,19 @@
  *@...: var input
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i, sum;
 
-	va_start(ap, n);
-
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-
+	{
+		sum = sum + va_arg(args, int);
+	}
+	va_end(args);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
