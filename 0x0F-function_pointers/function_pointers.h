48dee4897923382c8bdeff4863df04d11dbb788b
Taha <traitaha@gmail.com>
2023-08-16 18:50:30 -0700

    nc

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
 int op_sub(int a, int b);
